ecfd8cbbc062a1c9065be644d9b8b6b95f6099b7
Evan Follman <evan.follman@gmail.com>
2025-12-15 11:36:22 -0600

    new file:   day1/day1-1.jl 	new file:   day1/day1-1.txt 	new file:   day1/day1-1ex.txt 	new file:   day1/day1-2.jl

---
+++ day1/day1-2.jl
@@ -0,0 +1,51 @@
+global const currDIR::String = @__DIR__
+
+function passfunc(filedir::String)
+    open(filedir) do file
+
+        counter = 0
+        dial = 50
+
+        while !eof(file)
+            currLine = readline(file)
+            value = parse(Int,currLine[2:end])
+            sign = 0
+
+            #Lol this bad fix fr fr
+            if currLine[1] == 'L'
+                sign = -1
+            elseif currLine[1] == 'R'
+                sign = 1
+            else
+                @error "Not L or R???"
+            end
+
+            for i = 1:value
+                dial += 1*sign
+                if dial == 0 || dial == 100
+                    counter += 1
+                end
+                dial = mod(dial,100)
+            end
+            #println(counter)
+            #if dial == 0
+            #    counter += 1
+            #end
+        end
+        return counter
+    end
+end
+
+function run()
+    test = passfunc("$currDIR/day1-1ex.txt")
+    if test == 6
+        println("Test ex passed: $test")
+    end
+    @time test = passfunc("$currDIR/day1-1.txt")
+    if test == 6738
+        println("Test real passed: $test")
+    end
+    return nothing
+end
+
+run()
